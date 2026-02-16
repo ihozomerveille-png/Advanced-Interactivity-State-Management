4c334a0165fe90b3f009130f4c9f3b67e045ec9c
lenovo <ihozomerveille@gmail.com>
2026-02-16 09:48:00 +0200

    editing

--- images/script.js
+++ images/script.js
@@ -1,11 +1,18 @@
-function toggleComments(){
+// Open first replies by default
+document.getElementById("myBtn").click();
 
-let comments = document.getElementById("comments");
-
-if(comments.style.display === "none"){
-comments.style.display="block";
-}else{
-comments.style.display="none";
+// Toggle blog replies
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.style.display === "none" || x.style.display === "") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
+// Like button
+function likeFunction(x) {
+  x.style.fontWeight = "bold";
+  x.innerHTML = " Liked";
 }
