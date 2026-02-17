071ec92cf10b0527cda9e9ff3079ed51567f25f8
lenovo <ihozomerveille@gmail.com>
2026-02-17 09:03:41 +0200

    saving changes

--- images/script.js
+++ images/script.js
@@ -1,18 +1,6 @@
-// Open first replies by default
-document.getElementById("myBtn").click();
-
-// Toggle blog replies
-function myFunction(id) {
-  var x = document.getElementById(id);
-  if (x.style.display === "none" || x.style.display === "") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
-// Like button
-function likeFunction(x) {
-  x.style.fontWeight = "bold";
-  x.innerHTML = " Liked";
-}
+// Like button interaction
+document.querySelectorAll(".like").forEach(btn=>{
+btn.addEventListener("click",()=>{
+alert("You liked this post!");
+});
+});
